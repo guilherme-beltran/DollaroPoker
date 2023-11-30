cc3226458531f90198e4b9566b9af3c85c6435a3
guilhermessjogos <gtmbeltran@gmail.com>
2023-11-30 15:45:37 -0300

    [new] adicionado healthcheck

---
+++ Backoffice.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backoffice.API.Controllers;
+
+[ApiController]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult> Get()
+    {
+        return Ok($"Funcionando!");
+    }
+}
